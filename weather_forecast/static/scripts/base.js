d7f573d9f1d4026248514b0da0a2ee78041a34e6
Egor <egor_onishchuk@mail.ru>
2024-05-25 15:18:45 +0300

    Декомпозированы статические файлы

--- weather_forecast/static/scripts/base.js
+++
@@ -1,9 +0,0 @@
-const tooltipTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="tooltip"]',
-);
-const tooltipList = [...tooltipTriggerList].map(
-  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl),
-);
-
-document.querySelector(".footer__copyright").innerText +=
-  `, ${new Date().getFullYear()}`;
